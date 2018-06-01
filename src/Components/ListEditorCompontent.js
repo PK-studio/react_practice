9b9755e2a3c8181344b8b9d820bf0e7c5192df4c
Patryk Karwowski <vb.karwowski@gmail.com>
2018-06-01 17:36:39 +0100

    item list continuation, remove functionality is still not working

--- src/Components/ListEditorCompontent.js
+++ src/Components/ListEditorCompontent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 class ListEditorCompontent extends Component {
-
   constructor(props) {
     super(props)
     this.state = {
@@ -10,8 +9,8 @@ class ListEditorCompontent extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.addHandler = this.addHandler.bind(this);
+    this.ListEditorUpdater = this.ListEditorUpdater.bind(this);
   };
-
   render() {
     return (
       <div>
@@ -28,16 +27,14 @@ class ListEditorCompontent extends Component {
             />
             <button onClick={this.addHandler} > add </button>
           </form>
-          {/* <ItemsList/> */}
+          <ItemsList items={this.state.items} updater={this.ListEditorUpdater}/>
         </div>
       </div>
     );
   }
-
   handleChange(event){
     this.setState({text: event.target.value})
   }
-
   addHandler(event){
     event.preventDefault();
     if (!this.state.text.length) {
@@ -50,17 +47,41 @@ class ListEditorCompontent extends Component {
       items: prevState.items.concat(myItem),
       text: ""
     }))
-
-    // this.setState is asynchronous = value doesn't match
-    // this.setState({ 'updated': 'state'}, () => {
-    //   console.log(this.state.updated);
-    // });
+  }
+  ListEditorUpdater(nexItemsValue){
+    this.setState({items: nexItemsValue})
   }
 }
-// class ItemsList extends Component {
-//   render(){
 
-//   }
-// }
+class ItemsList extends Component {
+  constructor(props){
+    super(props)
+    this.removeItem = this.removeItem.bind(this)
+  }
+  render() {
+    return(
+      <ul>
+        {this.props.items.map((oneOfitem, index) => (
+          <li 
+            data-number={index} 
+            key={index}
+          >
+            <span><b>{oneOfitem.text} </b></span>
+            <button onClick={this.removeItem}> remove</button>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+  removeItem(event){
+    let domEl = event.target.parentElement
+    let indexOfdomEl = domEl.getAttribute("data-number")
+    domEl.remove()
+    let newITemList = this.props.items.splice(indexOfdomEl, 1)
+    console.log(this.props.items)
+    console.log(newITemList)
+    this.props.updater(this.props.items)
+  }
+}
 
 export default ListEditorCompontent;
